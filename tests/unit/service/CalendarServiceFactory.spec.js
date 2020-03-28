b444011058190dec7d9f4412184bfb9450b04cbe
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-03-28 15:24:10 +0100

    Enhance test

--- tests/unit/service/CalendarServiceFactory.spec.js
+++ tests/unit/service/CalendarServiceFactory.spec.js
@@ -1,9 +1,18 @@
 import { CalendarServiceFactory } from '@/service/CalendarServiceFactory'
+import { CalendarServiceMock } from '@/service/CalendarServiceMock'
+import { CalendarService } from '@/service/CalendarService'
 
 describe('CalendarServiceFactory.js', () => {
-  it('it returns a mock in test mode', () => {
+  it('returns a mock in test mode', () => {
     process.env.NODE_ENV = 'test'
     const serviceMock = new CalendarServiceFactory().create()
-    expect(Object.prototype.toString.call(serviceMock)).toMatch('[object Object]')
+    expect(CalendarServiceMock.prototype.isPrototypeOf(serviceMock)).toBeTruthy()
+    expect(CalendarService.prototype.isPrototypeOf(serviceMock)).toBeTruthy()
+  }),
+  
+  it('return the service class when not in test mode', () => {
+    process.env.NODE_ENV = 'production'
+    const serviceMock = new CalendarServiceFactory().create()
+    expect(CalendarService.prototype.isPrototypeOf(serviceMock)).toBeTruthy()
   })
 })
