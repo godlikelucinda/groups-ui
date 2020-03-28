9624e2bcb3f90eacaab1f4e39cfe2d551fbf886b
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-03-28 23:01:30 +0100

    Tests for CalenderServiceMock

--- tests/unit/service/CalendarServiceFactory.spec.js
+++ tests/unit/service/CalendarServiceFactory.spec.js
@@ -4,19 +4,12 @@ describe('CalendarServiceFactory.js', () => {
   it('returns a mock in test mode', () => {
     process.env.NODE_ENV = 'test'
     const serviceMock = new CalendarServiceFactory().create()
-    expect(serviceMock.constructor.name).toMatch('CalendarServiceMock')
-  })
-
-  it('returns a mock in test mode which is a subclass of the service class', () => {
-    process.env.NODE_ENV = 'test'
-    const serviceMock = new CalendarServiceFactory().create()
-    expect(serviceMock.constructor.name).toBe('CalendarService')
     expect(serviceMock.constructor.name).toBe('CalendarServiceMock')
   })
 
   it('return the service class when not in test mode', () => {
     process.env.NODE_ENV = 'production'
     const serviceMock = new CalendarServiceFactory().create()
-    expect(serviceMock.constructor.name).toMatch('CalendarService')
+    expect(serviceMock.constructor.name).toBe('CalendarService')
   })
 })
--- tests/unit/service/CalenderServiceMock.spec.js
+++ tests/unit/service/CalenderServiceMock.spec.js
@@ -4,7 +4,18 @@ describe('CalendarServiceMock.js', () => {
   it('it does something', () => {
     const serviceMock = new CalendarServiceMock()
     const events = serviceMock.getEvents()
+
     expect(events.length).toBe(3)
-    expect(events[0].eventId).toMatch('001')
+
+    const bradBirthday = events[0]
+    const bradDoctorAppointment = events[1]
+    const bradSoccerTraining = events[2]
+
+    expect(events[0].eventId).toBe('001')
+    expect(events[0].title).toBe('Geburtstag Brad')
+    expect(events[1].eventId).toBe('002')
+    expect(events[1].title).toBe('Brad Hautarzt')
+    expect(events[2].eventId).toBe('003')
+    expect(events[2].title).toBe('Brad Fußballverein')
   })
 })
