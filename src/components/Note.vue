38d397e21e67ac4608e59e39ad0ec285793d46ba
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-04-02 22:06:34 +0200

    Add example for avatars

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -4,6 +4,12 @@
     <p><b>{{ noteObject.title }}</b></p>
     <p>{{ noteObject.startDateTime }} - {{ noteObject.endDateTime }}</p>
     <p>{{ noteObject.description }}</p>
+    <div class="mb-4">
+      <b-avatar></b-avatar>
+      <b-avatar variant="primary" text="BV"></b-avatar>
+      <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+      <b-avatar variant="success" icon="people-fill"></b-avatar>
+    </div>
   </div>
 </template>
 
@@ -18,3 +24,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.note {
+  text-align: left;
+}
+</style>
