42f386000e3b3edeefc90c5805d565e193af1035
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-04-14 17:43:35 +0200

    Add modal dialog

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="note">
-    <hr>
-    <p><b>{{ noteObject.title }}</b></p>
-    <p>{{ noteObject.startDateTime }} - {{ noteObject.endDateTime }}</p>
-    <p>{{ noteObject.description }}</p>
-    <div class="mb-4">
-      <b-avatar></b-avatar>
-      <b-avatar variant="primary" text="BV"></b-avatar>
-      <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
-      <b-avatar variant="success" icon="people-fill"></b-avatar>
+  <div>
+    <div v-b-modal="noteObject.noteId" class="note">
+      <hr>
+      <p><b>{{ noteObject.title }}</b></p>
+      <p>{{ noteObject.startDateTime }} - {{ noteObject.endDateTime }}</p>
+      <p>{{ noteObject.description }}</p>
+      <div class="mb-4">
+        <b-avatar></b-avatar>
+        <b-avatar variant="primary" text="BV"></b-avatar>
+        <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+        <b-avatar variant="success" icon="people-fill"></b-avatar>
+      </div>
     </div>
+
+    <b-modal :id="noteObject.noteId" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+      <Note :noteObject="noteObject"></Note>
+    </b-modal>
   </div>
 </template>
 
