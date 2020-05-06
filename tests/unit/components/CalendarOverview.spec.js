1e798d7feea0fc357bf9adbdb82e8bb44db42223
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-05-06 18:38:28 +0200

    Improve CalenderOverview test (wip)

--- tests/unit/components/CalendarOverview.spec.js
+++ tests/unit/components/CalendarOverview.spec.js
@@ -17,9 +17,18 @@ describe('CalendarOverview.vue', () => {
     const wrapper = shallowMount(CalendarOverview, {
       propsData: { noteService }
     })
-    await wrapper.vm.$nextTick()
-    // expect(wrapper.contains(Note)).toBe(true)
-    expect(wrapper.html()).toContain('circle').toBe(true)
-    // expect(wrapper.html().tocontains('.b-calendar')).toBe(true)
+    
+    const getFirstOfMarch = jest.fn(() => { return new Date(2020, 3, 1) })
+    wrapper.setMethods({ getActiveDate: getFirstOfMarch })
+    expect(wrapper.vm.getActiveDate().toString()).toBe(new Date(2020, 3, 1).toString())
+    expect(wrapper.text()).toContain('Keine Einträge in diesem Monat vorhanden')
+
+    const getNewYear = jest.fn(() => { return new Date(2020, 1, 1) })
+    wrapper.setMethods({ getActiveDate: getNewYear })
+    expect(wrapper.vm.getActiveDate().toString()).toBe(new Date(2020, 1, 1).toString())
+    // expect(wrapper.vm.getNotesInSameMonth(wrapper.vm.getActiveDate()).length).toBe(3)
+    // expect(wrapper.text()).toContain('Geburtstagsfeier im Indoorspielplatz')
+    // expect(wrapper.text()).toContain('Brad Hautarzt')
+    // expect(wrapper.text()).toContain('Brad Fußballverein')
   })
 })
